b709792f36585c61e82195c02190f2408c128a05
meltstrap <meltstrap@uiiverse.xyz>
2019-10-02 20:29:45 -0300

    Added files

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<link href="css/main.css" rel="stylesheet">
+    <title>Uiiverse for Nintendo Switch</title>
+</head>
+<body>
+    <h2>Uiiverse for Nintendo Switch</h2>
+    <p>Uiiverse is a re-creation of Nintendo's well-known social network, Miiverse. It allows you to interact with fellow Nintendo fans, give Yeahs to posts, and share screenshots of your favorite games. Now available on Nintendo Switch.</p>
+    <p><a href="https://switchbru.com/appstore/#/app/Uiiverse">SwitchBru</a> / <a href="https://downloads.uiiverse.xyz/switch/latest/Uiiverse.zip">Direct</a> / <a href="https://uiiverse.miro.gg/switch">Mirror</a> / <a href="https://github.com/uiiverse/Switch/releases/latest">GitHub</a></p>
+<a href="/">« Go back to Uiiverse</a>
+</body>
+</html>
